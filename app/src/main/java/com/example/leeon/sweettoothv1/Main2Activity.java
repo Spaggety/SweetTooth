42637c1ee31c6aa4bb2d27a8d92a7f2e3a51840d
Spaggety <andresjoseis@gmail.com>
2017-11-04 16:08:09 -0700

    made a working gui

--- app/src/main/java/com/example/leeon/sweettoothv1/Main2Activity.java
+++ app/src/main/java/com/example/leeon/sweettoothv1/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.example.leeon.sweettoothv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.example.leeon.sweettoothv1.R.id.textView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        counter(101);
+    }
+
+    protected void counter(int a){
+
+        for(int i = 0;i < a; i++){
+            if(i%5 == 0){
+                String stringy = " ahsdas";
+                TextView help = (TextView) findViewById(textView);
+                help.setText(stringy);
+            }
+            else{
+                TextView help = (TextView) findViewById(textView);
+                help.setText(" ");
+            }
+        }
+
+    }
+
+
+
+
+    public void oldPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/leeon/sweettoothv1/MainActivity.java
+++ app/src/main/java/com/example/leeon/sweettoothv1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.leeon.sweettoothv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
+    public void newPage(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
 }
